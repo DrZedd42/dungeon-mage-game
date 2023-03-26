a1eaf54a41b6c146ab3d770ba25d7d91d79711f9
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-03-26 03:21:19 +0200

    first commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Dungeon Mage',
+    version: '1.0',
+    width: 1280,
+    height: 720,
+    type: Phaser.AUTO,
+    scene: [],
+    render: { pixelArt: true, antialias: false },
+};
