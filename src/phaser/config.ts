d1f2d835d65409ffb5bc13aa99f252f595d5680e
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-03-28 23:36:55 +0200

    feat: use example test scene as starter scene

--- src/phaser/config.ts
+++ src/phaser/config.ts
@@ -1,8 +1,10 @@
+import { TestScenePhaser } from './phaser scene/scenes/test-scene.phaser';
+
 export const GameConfig: Phaser.Types.Core.GameConfig = {
     title: 'Dungeon Mage',
     version: '1.0',
     width: 1280,
     height: 720,
-    scene: [],
+    scene: [TestScenePhaser],
     render: { pixelArt: true, antialias: false },
 };
