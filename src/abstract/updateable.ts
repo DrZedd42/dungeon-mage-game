8b21082f731c4c4609668c0331bb9c0add547e7b
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-04-01 17:03:38 +0200

    feat: set updateStack as private

--- src/abstract/updateable.ts
+++ src/abstract/updateable.ts
@@ -1,4 +1,22 @@
 export abstract class Updateable {
-    protected updateStack: Array<Function> = [];
+    private updateStack: Array<Function> = [];
+    addToUpdate(func: Function) {
+        this.updateStack.push(func);
+    }
+
+    checkUpdatesEmpty() {
+        return this.updateStack.length <= 0;
+    }
+
+    getNextUpdate(): Function {
+        let func = this.updateStack.pop();
+        if (func instanceof Function) {
+            return func;
+        } else {
+            throw new Error(
+                "Update Stack was empty, please check with 'checkUpdatesEmpty' before calling this function."
+            );
+        }
+    }
     abstract update(_delta: number): void;
 }
