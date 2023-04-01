77b678dfb7e4520d85ce10499977fb1f280c807a
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-04-01 14:38:34 +0200

    docs: add comments explaining use-cases

--- src/abstract/input-controller.ts
+++ src/abstract/input-controller.ts
@@ -1,10 +1,10 @@
-interface KeyInputProps {
+export interface KeyInputProps {
     down: boolean; // Should only be true only the first time the button is pressed until released
     press: boolean;
     up: boolean; // Should only be true the first time the button is released after it was pressed
 }
 
-interface DirectionalInputProps {
+export interface DirectionalInputProps {
     active: boolean;
     vector: { x: number; y: number };
 }
@@ -12,7 +12,7 @@ interface DirectionalInputProps {
 export interface InputController {
     getMovementDirection(): DirectionalInputProps; // Direction at which the controlled instance is moving towards
     getFacingDirection(): DirectionalInputProps; // Direction at which the controlled instance is facing (also the direction where the player will swing their item at)
-    getAction(): KeyInputProps; //
-    getInteraction(): KeyInputProps;
-    getStart(): KeyInputProps;
+    getAction(): KeyInputProps; // Used to use items, swing weapon or press buttons
+    getInteraction(): KeyInputProps; // Used to piuck up items, interact with objects in the game world (e.g NPCs, Furnace, ...)
+    getStart(): KeyInputProps; // Used to pause the game
 }
