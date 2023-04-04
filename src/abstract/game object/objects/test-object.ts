1d24e64b5fe145dd95c6b3cbb8b651d13568de70
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-04-05 00:29:13 +0200

    feat: add idle and run animation

--- src/abstract/game object/objects/test-object.ts
+++ src/abstract/game object/objects/test-object.ts
@@ -4,9 +4,14 @@ import { Sprite } from '../../sprite/sprite';
 import { GameObject } from '../game-object';
 
 export class TestObject extends GameObject {
+    private sprites = {
+        run: new Sprite('little_mage_run'),
+        idle: new Sprite('little_mage_idle'),
+    };
+
     constructor(_x: number, _y: number, _scene: GameScene) {
         super(_x, _y, _scene);
-        let sprite = new Sprite('testsprite');
+        let sprite = this.sprites.run;
         sprite.setDimension(50, 50);
         let collider = new RectCollider(
             this.getPosition().x,
@@ -22,11 +27,23 @@ export class TestObject extends GameObject {
     move(delta: number) {
         let input = this.getRelatedScene().getInputController();
         if (input.getMovementDirection().active) {
+            this.setSprite(this.sprites.run, true);
             this.moveTowardsDirection(
                 input.getMovementDirection().degree,
-                1 * delta,
+                0.25 * delta,
                 'degree'
             );
+            if (this.getSprite()) {
+                this.getSprite()!.setScale(
+                    input.getMovementDirection().degree < -90 ||
+                        input.getMovementDirection().degree > 90
+                        ? -1
+                        : 1,
+                    1
+                );
+            }
+        } else {
+            this.setSprite(this.sprites.idle, true);
         }
     }
 }
