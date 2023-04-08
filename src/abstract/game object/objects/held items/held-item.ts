54d9f6e26f1375d6b54681cf37068ad351b40a70
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-04-08 13:08:18 +0200

    feat: remove wielder from held item

--- src/abstract/game object/objects/held items/held-item.ts
+++ src/abstract/game object/objects/held items/held-item.ts
@@ -3,23 +3,11 @@ import { Sprite } from '../../../sprite/sprite';
 import { GameObject } from '../../game-object';
 
 export abstract class HeldItem extends GameObject {
-    private wielder: GameObject;
-    constructor(
-        _x: number,
-        _y: number,
-        _scene: GameScene,
-        _wielder: GameObject,
-        _name: string
-    ) {
+    constructor(_x: number, _y: number, _scene: GameScene, _name: string) {
         super(_x, _y, _scene);
-        this.wielder = _wielder;
         let sprite = new Sprite(_name);
         this.setSprite(sprite);
         this.rotate(-135);
-    }
-
-    setWielder(_wielder: GameObject) {
-        this.wielder = _wielder;
     }
 
     rotate(_rotation: number) {
